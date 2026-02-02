827ed781ebfb9a91d453517d5174b5e9f9846799
srishti Raghava <srishtiraghava1@gmail.com>
2026-02-02 13:44:37 +0530

    ml added

--- ml/riskEngine.js
+++
@@ -1,7 +0,0 @@
-module.exports=function calculateRisk({ heartRate,spo2,skinTemp}){
-    let risk = 0;
-    if (heartRate>110||heartRate<50)risk+=30;
-    if(spo2<92)risk+=40;
-    if (skinTemp>38)risk+=30;
-    return Math.min(risk,100);
-};
